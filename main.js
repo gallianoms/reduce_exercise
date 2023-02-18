d91bc3f6d7ebebd86a6bc988844b121dcf46a0a3
gallianoms <ggallianoms@gmail.com>
2023-02-18 12:36:03 -0500

    completed exercise 4

--- main.js
+++ main.js
@@ -26,4 +26,26 @@ var strConcat = function () {
     }
     return numbers.reduce(function (acc, num) { return acc.concat(num.toString()); }, '');
 };
-console.log(strConcat(1, 2, 3, 4, 5)); // "12345"
+// console.log(strConcat(1, 2, 3, 4, 5)) // "12345"
+//* Exercise 4
+//* Turn an array of voter objects into a count of how many people voted
+var voters = [
+    { name: 'Bob', age: 30, voted: true },
+    { name: 'Jake', age: 32, voted: true },
+    { name: 'Kate', age: 25, voted: false },
+    { name: 'Sam', age: 20, voted: false },
+    { name: 'Phil', age: 21, voted: true },
+    { name: 'Ed', age: 55, voted: true },
+    { name: 'Tami', age: 54, voted: true },
+    { name: 'Mary', age: 31, voted: false },
+    { name: 'Becky', age: 43, voted: false },
+    { name: 'Joey', age: 41, voted: true },
+    { name: 'Jeff', age: 30, voted: true },
+    { name: 'Zack', age: 19, voted: false },
+];
+var totalVotes = function (voters) {
+    return voters
+        .filter(function (voter) { return voter.voted === true; })
+        .reduce(function (acc, voter) { return (acc += 1); }, 0);
+};
+console.log(totalVotes(voters)); // 7
