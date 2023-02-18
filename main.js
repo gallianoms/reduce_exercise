faf1fb383f7b0cc19771160aa2544c8619b9237f
gallianoms <ggallianoms@gmail.com>
2023-02-18 12:41:13 -0500

    completed exercise 6

--- main.js
+++ main.js
@@ -60,4 +60,66 @@ var wishlist = [
 var shoppingSpree = function (wishlist) {
     return wishlist.reduce(function (acc, item) { return acc + item.price; }, 0);
 };
-console.log(shoppingSpree(wishlist)); // 10
+// console.log(shoppingSpree(wishlist)) // 10
+//* Exercise 6
+//* Given an array of potential voters, return an object representing the results of the vote
+//* Include how many of the potential voters were in the ages 18-25, how many from 26-35, how many from 36-55, and how many of each of those age ranges actually voted. The resulting object containing this data should have 6 properties. See the example output at the bottom.
+var arrVoters = [
+    { name: 'Bob', age: 30, voted: true },
+    { name: 'Jake', age: 32, voted: true },
+    { name: 'Kate', age: 25, voted: false },
+    { name: 'Sam', age: 20, voted: false },
+    { name: 'Phil', age: 21, voted: true },
+    { name: 'Ed', age: 55, voted: true },
+    { name: 'Tami', age: 54, voted: true },
+    { name: 'Mary', age: 31, voted: false },
+    { name: 'Becky', age: 43, voted: false },
+    { name: 'Joey', age: 41, voted: true },
+    { name: 'Jeff', age: 30, voted: true },
+    { name: 'Zack', age: 19, voted: false },
+];
+var voterResults = function (arrVoters) {
+    var numYoungVotes = 0;
+    var numYoungPeople = 0;
+    var numMidVotesPeople = 0;
+    var numMidsPeople = 0;
+    var numOldVotesPeople = 0;
+    var numOldsPeople = 0;
+    return arrVoters.reduce(function (acc, voter) {
+        if (voter.age >= 18 && voter.age <= 25 && voter.voted === true) {
+            numYoungVotes += 1;
+        }
+        if (voter.age >= 18 && voter.age <= 25) {
+            numYoungPeople += 1;
+        }
+        if (voter.age >= 26 && voter.age <= 35 && voter.voted === true) {
+            numMidVotesPeople += 1;
+        }
+        if (voter.age >= 26 && voter.age <= 35) {
+            numMidsPeople += 1;
+        }
+        if (voter.age >= 36 && voter.age <= 55 && voter.voted === true) {
+            numOldVotesPeople += 1;
+        }
+        if (voter.age >= 36 && voter.age <= 55) {
+            numOldsPeople += 1;
+        }
+        return (acc = {
+            numYoungVotes: numYoungVotes,
+            numYoungPeople: numYoungPeople,
+            numMidVotesPeople: numMidVotesPeople,
+            numMidsPeople: numMidsPeople,
+            numOldVotesPeople: numOldVotesPeople,
+            numOldsPeople: numOldsPeople
+        });
+    }, {});
+};
+console.log(voterResults(arrVoters));
+// {
+//  numYoungVotes: 1
+//  numYoungPeople: 4
+//  numMidVotesPeople: 3
+//  numMidsPeople: 4
+//  numOldVotesPeople: 3
+//  numOldsPeople: 4
+// }
--- main.ts
+++ main.ts
@@ -75,4 +75,83 @@ const shoppingSpree = (wishlist: Wishlist[]) => {
   return wishlist.reduce((acc, item) => acc + item.price, 0)
 }
 
-console.log(shoppingSpree(wishlist)) // 10
+// console.log(shoppingSpree(wishlist)) // 10
+
+//* Exercise 6
+//* Given an array of potential voters, return an object representing the results of the vote
+//* Include how many of the potential voters were in the ages 18-25, how many from 26-35, how many from 36-55, and how many of each of those age ranges actually voted. The resulting object containing this data should have 6 properties. See the example output at the bottom.
+
+var arrVoters: Vote[] = [
+  { name: 'Bob', age: 30, voted: true },
+  { name: 'Jake', age: 32, voted: true },
+  { name: 'Kate', age: 25, voted: false },
+  { name: 'Sam', age: 20, voted: false },
+  { name: 'Phil', age: 21, voted: true },
+  { name: 'Ed', age: 55, voted: true },
+  { name: 'Tami', age: 54, voted: true },
+  { name: 'Mary', age: 31, voted: false },
+  { name: 'Becky', age: 43, voted: false },
+  { name: 'Joey', age: 41, voted: true },
+  { name: 'Jeff', age: 30, voted: true },
+  { name: 'Zack', age: 19, voted: false },
+]
+
+interface Result {
+  numYoungVotes: number
+  numYoungPeople: number
+  numMidVotesPeople: number
+  numMidsPeople: number
+  numOldVotesPeople: number
+  numOldsPeople: number
+}
+
+const voterResults = (arrVoters: Vote[]) => {
+  let numYoungVotes: number = 0
+  let numYoungPeople: number = 0
+
+  let numMidVotesPeople: number = 0
+  let numMidsPeople: number = 0
+
+  let numOldVotesPeople: number = 0
+  let numOldsPeople: number = 0
+
+  return arrVoters.reduce((acc, voter): Result => {
+    if (voter.age >= 18 && voter.age <= 25 && voter.voted === true) {
+      numYoungVotes += 1
+    }
+    if (voter.age >= 18 && voter.age <= 25) {
+      numYoungPeople += 1
+    }
+    if (voter.age >= 26 && voter.age <= 35 && voter.voted === true) {
+      numMidVotesPeople += 1
+    }
+    if (voter.age >= 26 && voter.age <= 35) {
+      numMidsPeople += 1
+    }
+    if (voter.age >= 36 && voter.age <= 55 && voter.voted === true) {
+      numOldVotesPeople += 1
+    }
+    if (voter.age >= 36 && voter.age <= 55) {
+      numOldsPeople += 1
+    }
+
+    return (acc = {
+      numYoungVotes,
+      numYoungPeople,
+      numMidVotesPeople,
+      numMidsPeople,
+      numOldVotesPeople,
+      numOldsPeople,
+    })
+  }, {})
+}
+
+console.log(voterResults(arrVoters))
+// {
+//  numYoungVotes: 1
+//  numYoungPeople: 4
+//  numMidVotesPeople: 3
+//  numMidsPeople: 4
+//  numOldVotesPeople: 3
+//  numOldsPeople: 4
+// }
